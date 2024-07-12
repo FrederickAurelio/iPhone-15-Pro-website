55a657efcf853d672e627ff892fbba1bf226cc91
Frederick Aurelio Halim <frederick.ah88@gmail.com>
2024-07-12 17:36:28 +0800

    Add files via upload

---
+++ src/utils/animations.js
@@ -0,0 +1,27 @@
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+
+export function animateWithGsapTimeline(timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps) {
+  timeline.to(rotationRef.current.rotation, {
+    y: rotationState,
+    duration: 1,
+    ease: "power2.inOut",
+  })
+
+  timeline.to(firstTarget, { ...animationProps, ease: "power2.inOut" }, "<")
+  timeline.to(secondTarget, { ...animationProps, ease: "power2.inOut" }, "<")
+}
+
+export function animateWithGsap(target, animationProps, scrollProps) {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverse",
+      start: "top 85%",
+      ...scrollProps
+    }
+  })
+}
